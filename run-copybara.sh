3a0862fd84f247621e811c93f0bc431db98cfaf8
jvincent-mongodb <jeffrey.vincent@mongodb.com>
2024-10-09 19:08:22 -0700

    simplify workflow for cloud-docs

--- run-copybara.sh
+++ run-copybara.sh
@@ -16,11 +16,6 @@
 
 set -Eeou pipefail
 
-if [[ "${TAG:?}" == "" ]]; then
-    echo "missing \$TAG"
-    exit 1
-fi
-
 if [[ "${GH_TOKEN:?}" == "" ]]; then
     echo "missing \$GH_TOKEN"
     exit 1
@@ -31,20 +26,10 @@ if [[ "${WORKFLOW:?}" == "" ]]; then
     exit 1
 fi
 
-if [[ "${EVERGREEN_USER:?}" == "" ]]; then
-    echo "missing \$EVERGREEN_USER"
-    exit 1
-fi
-
-if [[ "${EVERGREEN_API_KEY:?}" == "" ]]; then
-    echo "missing \$EVERGREEN_API_KEY"
-    exit 1
-fi
-
-if [[ "${DOCS_SLACK_CHANNEL:?}" == "" ]]; then
-    echo "missing \$DOCS_SLACK_CHANNEL"
-    exit 1
-fi
+# if [[ "${DOCS_SLACK_CHANNEL:?}" == "" ]]; then
+#     echo "missing \$DOCS_SLACK_CHANNEL"
+#     exit 1
+# fi
 
 cat <<EOF > .gitconfig
 [user]
@@ -73,10 +58,10 @@ PR_URL=$(docker logs copybara-container 2>&1 | grep "/pull/" | sed -E 's/^.*(htt
 rm -rf .git-credentials .gitconfig copy.bara.sky
 docker rm -f copybara-container
 
-TARGET="$DOCS_SLACK_CHANNEL"
-MSG="Hey team :wave: ${PR_URL} is ready for review :thankyou:"
-echo "{\"target\":\"$TARGET\",\"msg\":\"$MSG\"}"
-curl --header "Api-User:${EVERGREEN_USER:?}" \
-    --header "Api-Key:${EVERGREEN_API_KEY:?}" \
-    --request POST "https://evergreen.mongodb.com/rest/v2/notifications/slack" \
-    --data "{\"target\":\"$TARGET\",\"msg\":\"$MSG\"}"
+# TARGET="$DOCS_SLACK_CHANNEL"
+# MSG="Hey team :wave: ${PR_URL} is ready for review :thankyou:"
+# echo "{\"target\":\"$TARGET\",\"msg\":\"$MSG\"}"
+# curl --header "Api-User:${EVERGREEN_USER:?}" \
+#     --header "Api-Key:${EVERGREEN_API_KEY:?}" \
+#     --request POST "https://evergreen.mongodb.com/rest/v2/notifications/slack" \
+#     --data "{\"target\":\"$TARGET\",\"msg\":\"$MSG\"}"
